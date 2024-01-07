c1845c1ad109e0c5f6d763518a4fcd91a71a85df
Nicolas <nicolas.durpoix82@gmail.com>
2024-01-07 16:53:26 +0100

    Skills cache

--- src/api/skill.ts
+++ src/api/skill.ts
@@ -1,14 +1,7 @@
 import { client } from '@/lib/contentful';
 import { EntryCollection, EntrySkeletonType } from 'contentful';
 import { ISkill } from '../../contentfulTypes';
-import { NextApiRequest, NextApiResponse } from 'next';
-
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  res.setHeader('Cache-Control', 'no-cache')
-}
+export const fetchCache = 'force-no-store'
 export const getSkillBySlug = async (slug: string) => {
 
   const response = await client.getEntries({
