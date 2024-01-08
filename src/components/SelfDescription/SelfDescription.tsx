bdc703beb99891bb6b46b626382d9d682f7bfa69
Nicolas <nicolas.durpoix82@gmail.com>
2024-01-08 15:31:43 +0100

    Replaced shapes with pseudo elements

--- src/components/SelfDescription/SelfDescription.tsx
+++ src/components/SelfDescription/SelfDescription.tsx
@@ -7,32 +7,32 @@ import { documentToReactComponents as renderElement } from '@contentful/rich-tex
 import { Grid } from '@/components/UI';
 import { SeeMore } from '@/components/UI/Button';
 import { Route } from '@/lib/router';
-import { Circle } from '@/components/UI/BlurredShapes';
 
 export const SelfDescription = async () => {
   const selfDescription = await getSelfDescription()
 
   return (
-    <div className={cn('fullHeightFlexContainer')}>
-      <div className={'layoutWrapper'}>
-        <Grid>
-          <Grid item colStart={3} colSpan={8} className={'flex items-center gap-12'}>
-            <div className={cn(css.pictureWrapper)}>
-              <Image className={cn(css.pictureWrapperImg)} src={me} alt={'Picture of me'}/>
-              <Circle color={'#DC6D02'} size={240} bottom={-230} left={-250}/>
-              <Circle color={'#5714B0'} size={240} top={-130} left={-80}/>
-            </div>
-            <div className={css.descriptionWrapper}>
-              <p className={cn(css.descriptionTitle)}>{selfDescription.title}</p>
-              <div className={css.descriptionContentWrapper}>
-                <p className={css.descriptionSubtitle}>{selfDescription.subtitle}</p>
-                <>{renderElement(selfDescription.excerpt)}</>
-                <SeeMore to={Route.ABOUT_PAGE}/>
+    <>
+      <div className={'fullHeightFlexContainer hidden md:flex'}>
+        <div className={'layoutWrapper'}>
+          <Grid>
+            <Grid item colStart={3} colSpan={8} className={'flex items-center gap-12'}>
+              <div className={cn(css.pictureWrapper)}>
+                <Image className={cn(css.pictureWrapperImg)} src={me} alt={'Picture of me'}/>
               </div>
-            </div>
+              <div className={css.descriptionWrapper}>
+                <p className={cn(css.descriptionTitle)}>{selfDescription.title}</p>
+                <div className={css.descriptionContentWrapper}>
+                  <p className={css.descriptionSubtitle}>{selfDescription.subtitle}</p>
+                  <>{renderElement(selfDescription.excerpt)}</>
+                  <SeeMore to={Route.ABOUT_PAGE}/>
+                </div>
+              </div>
+            </Grid>
           </Grid>
-        </Grid>
+        </div>
       </div>
-    </div>
+      <div className={'flex flex-col md:hidden'}>Mobile</div>
+    </>
   )
 }
--- src/components/UI/BlurredShapes/index.ts
+++
@@ -1,5 +0,0 @@
-import Circle from './Circle'
-
-export {
-  Circle
-}
