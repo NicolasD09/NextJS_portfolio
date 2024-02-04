bc25a42f4385d684c6fc055afc1412e1af242e53
Nicolas <nicolas.durpoix82@gmail.com>
2024-02-04 16:53:26 +0100

    fdsqfsqfsdqf

--- src/api/selfDescription.ts
+++ src/api/selfDescription.ts
@@ -1,7 +1,7 @@
 import { getSelfDescriptionUrl, requestOptions } from '@/lib/contentful';
-import { ISelfDescription } from '../../contentfulTypes';
+import { SelfDescription } from '../../contentfulTypes';
 export const getSelfDescription = async () => {
   const data = await fetch(getSelfDescriptionUrl(), requestOptions).then(resp => resp.json())
 
-  return data.items[0].fields as ISelfDescription;
+  return data.items[0].fields as SelfDescription;
 };
