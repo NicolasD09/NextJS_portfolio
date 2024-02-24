df5408fd16a0e2d8601f1002ef2018362c52794c
Nicolas <nicolas.durpoix82@gmail.com>
2024-02-24 18:24:52 +0100

    Fixed hook

--- src/components/pages/Article/hooks/useRenderDocument.tsx
+++ src/components/pages/Article/hooks/useRenderDocument.tsx
@@ -1,11 +1,10 @@
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 import { Document } from '@contentful/rich-text-types';
-import { Asset } from 'contentful';
 import useRenderElementOptions from '@/components/pages/Article/hooks/useRenderElementOptions';
 
 
-const useRenderDocument = (assets: Map<string, Asset> | undefined = undefined) => {
-  const options = useRenderElementOptions(assets)
+const useRenderDocument = () => {
+  const options = useRenderElementOptions()
   const renderElement = (document: Document) => {
     return documentToReactComponents(document, options)
   }
